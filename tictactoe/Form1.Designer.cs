b33c37e717dceb5f31274d0de85dea254b938f80
Deshai H <shai114@hotmail.com>
2021-04-05 08:27:24 -0500

    create

---
+++ tictactoe/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace tictactoe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(29, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1076, 1043);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1236, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Player";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(1211, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 163);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1236, 776);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "ignore";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1442, 1165);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ tictactoe/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tictactoe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            genButtons();
+        }
+        bool gameover = false;
+
+        Button[,] buttons = new Button[3, 3];
+
+        private void genButtons()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Size = new Size(300, 300);
+                    buttons[i, j].Location = new Point(i * 300, j * 300);
+                    buttons[i, j].FlatStyle = FlatStyle.Flat;
+                    buttons[i, j].Font = new System.Drawing.Font(DefaultFont.FontFamily, 80, FontStyle.Bold);
+                    buttons[i, j].Click += new EventHandler(button_c);
+
+                    panel1.Controls.Add(buttons[i, j]);
+
+                }
+            }
+        }
+
+       
+
+        void button_c(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button.Text != "")
+            {
+                return;
+            }
+
+            button.Text = button1.Text;
+            switchPlayer();
+            if(gameover == true)
+            {
+                gameover = false;
+            }
+        }
+
+        private void switchPlayer()
+        {
+            checkIfgameEnds();
+            if (button1.Text == "X" && gameover == false)
+            {
+                button1.Text = "O";
+            }
+            else if (button1.Text != "X" && gameover == false)
+            {
+                button1.Text = "X";
+            }
+        }
+
+        private void checkIfgameEnds()
+        {
+            List<Button> winnerButtons = new List<Button>();
+
+            //vertical win
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    if(buttons[i, j].Text != button1.Text)
+                    {
+                        break;
+                    }
+                    winnerButtons.Add(buttons[i, j]);
+                    if (j == 2) 
+                    {
+                        showwinner(winnerButtons);
+                        return;
+                    }
+                }
+            }
+            //horizontal win
+            for (int i = 0; i < 3; i++)
+            {
+                winnerButtons = new List<Button>();
+                for(int j = 0; j < 3; j++)
+                {
+                    if(buttons[j, i].Text != button1.Text)
+                    {
+                        break;
+                    }
+                    winnerButtons.Add(buttons[j, i]);
+                    if(j == 2)
+                    {
+                        showwinner(winnerButtons);
+                        return;
+                    }
+                }
+            }
+            //diagonal win
+            winnerButtons = new List<Button>();
+            for(int i = 0, j = 0; i < 3; i++, j++)
+            {
+                if(buttons[i, j].Text != button1.Text)
+                {
+                    break;
+                }
+                winnerButtons.Add(buttons[i, j]);
+                if(j ==2)
+                {
+                    showwinner(winnerButtons);
+                    return;
+                }
+            }
+            //diagonal win
+            winnerButtons = new List<Button>();
+            for (int i = 2, j = 0; j < 3; i--, j++)
+            {
+                if (buttons[i, j].Text != button1.Text)
+                {
+                    break;
+                }
+                winnerButtons.Add(buttons[i, j]);
+                if (j == 2)
+                {
+                    showwinner(winnerButtons);
+                    return;
+                }
+            }
+
+            foreach(var button in buttons)
+            {
+                if (button.Text == "")
+                    return;
+            }
+            MessageBox.Show("Tie Game!");
+            gameover = true;
+            Reset();
+        }
+        private void showwinner(List<Button> winnerButtons)
+        {
+            gameover = true;
+            if (button1.Text == "X")
+            {
+                foreach (var button in winnerButtons)
+                {
+                    button.BackColor = Color.Red;
+                }
+            }
+            else foreach (var button in winnerButtons)
+                {
+                    button.BackColor = Color.Blue;
+                }
+
+            MessageBox.Show("Player " + button1.Text + " wins");
+            Reset();
+
+        }
+
+        private void Reset()
+        {
+            this.Controls.Clear();
+            this.InitializeComponent();
+            genButtons();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //button2.Text = buttons[2, ].ToString(); [2,2] final square
+        }
+    }
+}
