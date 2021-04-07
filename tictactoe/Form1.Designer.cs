08c972edb14c9add4d558f59381a822ac09f1bbd
Deshai H <shai114@hotmail.com>
2021-04-06 22:31:23 -0500

    ai updates. doesn't work yet

--- tictactoe/Form1.Designer.cs
+++ tictactoe/Form1.Designer.cs
@@ -33,20 +33,22 @@ namespace tictactoe
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.panel1.Location = new System.Drawing.Point(43, 68);
+            this.panel1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.panel1.Location = new System.Drawing.Point(119, 106);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(505, 460);
+            this.panel1.Size = new System.Drawing.Size(650, 648);
             this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(676, 45);
+            this.label1.Location = new System.Drawing.Point(933, 106);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 37);
             this.label1.TabIndex = 1;
@@ -55,16 +57,16 @@ namespace tictactoe
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(659, 128);
+            this.button1.Location = new System.Drawing.Point(933, 195);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 163);
+            this.button1.Size = new System.Drawing.Size(233, 225);
             this.button1.TabIndex = 2;
             this.button1.Text = "X";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(659, 390);
+            this.button2.Location = new System.Drawing.Point(933, 552);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(169, 52);
             this.button2.TabIndex = 3;
@@ -72,11 +74,21 @@ namespace tictactoe
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1227, 496);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 37);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1077, 668);
+            this.ClientSize = new System.Drawing.Size(1462, 813);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -94,6 +106,7 @@ namespace tictactoe
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- tictactoe/Form1.cs
+++ tictactoe/Form1.cs
@@ -16,15 +16,28 @@ namespace tictactoe
         {
             InitializeComponent();
             genButtons();
+
+
         }
+        string[,] board = new string[,]
+        {
+            {"", "", ""},
+            {"", "", ""},
+            {"", "", ""},
+        };
+
         bool gameover = false;
+        int depth;
+        string checker;
+        string ai = "X";
+        string human = "O";
 
         Button[,] buttons = new Button[3, 3];
-
+        
         private void genButtons()
         {
             for (int i = 0; i < 3; i++)
-            {
+            { 
                 for (int j = 0; j < 3; j++)
                 {
                     buttons[i, j] = new Button();
@@ -33,9 +46,10 @@ namespace tictactoe
                     buttons[i, j].FlatStyle = FlatStyle.Flat;
                     buttons[i, j].Font = new System.Drawing.Font(DefaultFont.FontFamily, 41, FontStyle.Bold);
                     buttons[i, j].Click += new EventHandler(button_c);
-
+                    //buttons[i, j].TextAlign = ContentAlignment.TopCenter;
+                    
                     panel1.Controls.Add(buttons[i, j]);
-
+                    
                 }
             }
         }
@@ -61,35 +75,44 @@ namespace tictactoe
 
         private void switchPlayer()
         {
+
             checkIfgameEnds();
-            if (button1.Text == "X" && gameover == false)
+            if (button1.Text == ai && gameover == false)
             {
-                button1.Text = "O";
+                button1.Text = human;
             }
-            else if (button1.Text != "X" && gameover == false)
+            else if (button1.Text != ai && gameover == false)
             {
-                button1.Text = "X";
+                button1.Text = ai;
             }
+
+            if (button1.Text == ai)
+            {
+                label2.Text = ai + "'s turn";
+            }
+            else label2.Text = human + "'s turn";
         }
 
-        private void checkIfgameEnds()
+        private string checkIfgameEnds()
         {
             List<Button> winnerButtons = new List<Button>();
+           string winner = null;
 
             //vertical win
             for (int i = 0; i < 3; i++)
             {
                 for(int j = 0; j < 3; j++)
                 {
-                    if(buttons[i, j].Text != button1.Text)
+                    if(board[i,j] != button1.Text)
                     {
                         break;
                     }
                     winnerButtons.Add(buttons[i, j]);
-                    if (j == 2) 
+                    if (j == 2)
                     {
-                        showwinner(winnerButtons);
-                        return;
+                        //showwinner(winnerButtons);
+                        winner = board[0, i];
+
                     }
                 }
             }
@@ -99,15 +122,16 @@ namespace tictactoe
                 winnerButtons = new List<Button>();
                 for(int j = 0; j < 3; j++)
                 {
-                    if(buttons[j, i].Text != button1.Text)
+                    if(board[j, i] != button1.Text)
                     {
                         break;
                     }
                     winnerButtons.Add(buttons[j, i]);
                     if(j == 2)
                     {
-                        showwinner(winnerButtons);
-                        return;
+                        //showwinner(winnerButtons);
+                        winner = board[i, 0];
+
                     }
                 }
             }
@@ -123,7 +147,8 @@ namespace tictactoe
                 if(j ==2)
                 {
                     showwinner(winnerButtons);
-                    return;
+                    winner = board[0,0];
+
                 }
             }
             //diagonal win
@@ -138,18 +163,23 @@ namespace tictactoe
                 if (j == 2)
                 {
                     showwinner(winnerButtons);
-                    return;
+                    winner = board[2, 0];
                 }
             }
 
             foreach(var button in buttons)
             {
                 if (button.Text == "")
-                    return;
+                    return winner;
+                    
             }
+            
             MessageBox.Show("Tie Game!");
             gameover = true;
-            Reset();
+            return "Tie";
+            //Reset();
+            
+
         }
         private void showwinner(List<Button> winnerButtons)
         {
@@ -176,12 +206,108 @@ namespace tictactoe
             this.Controls.Clear();
             this.InitializeComponent();
             genButtons();
+            gameover = false;
 
+            
+        }
+
+        void best()
+        {
+            int bestscore = -0;
+            int[] move = { 0, 0 };
+            for (int i = 0; i < 3; i++)
+            {
+
+                for (int j = 0; j < 3; j++)
+                {
+                    if (gameover == true)
+                    { return; }
+                    if (board[i, j] == "")
+                    {
+                        board[i, j] = ai;
+                        int score = minimax(board, 0, false);
+                        //if (score == 0) { return; }
+                        board[i, j] = "";
+                        if (score > bestscore)
+                        {
+                            bestscore = score;
+                            move[0] = i;
+                            move[1] = j;
+                        }
+                    }
+                }
+            }
+            if (gameover == true)
+            { return; }
+            board[move[0], move[1]] = ai;
+            buttons[move[0], move[1]].Text = ai;
+            switchPlayer();
+        }
+
+        
+
+        public int minimax(string[,] board, int depth, bool maxx)
+        {            
+            string result = checkIfgameEnds();
+
+            int bestscore = -0;
+            var hello = new Dictionary<string, int>();
+            hello["X"] = 10;
+            hello["O"] = -10;
+            hello["Tie"] = 0;
+            
+            
+            if (result != null)
+            {
+                return hello[checkIfgameEnds()];      //result comes out as "" which is wrong and causes error
+            }
+
+            if (maxx) 
+            {
+                bestscore = -0;
+                for(int i = 0; i < 3; i++)
+                {
+                    for(int j = 0; j < 3; j++)
+                    {
+                        if(board[i, j] == "")
+                        {
+                            board[i, j] = ai;
+                            //button1.Text = human;
+                            int score = minimax(board, depth + 1, false);
+                            buttons[i, j].Text = "";
+                            board[i, j] = "";
+                            bestscore = Math.Max(score, bestscore);
+                            
+                        }
+                    }
+                }
+                return bestscore;
+            }
+            else {
+                bestscore = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (board[i, j] == "")
+                        {
+                            board[i, j] = human;
+                            int score = minimax(board, depth + 1, true);
+                            board[i, j] = "";
+                            bestscore = Math.Min(score, bestscore);
+
+                        }
+                    }
+                }
+            }
+            return bestscore;
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //button2.Text = buttons[2, ].ToString(); [2,2] final square
+            best();
+            
         }
     }
 }
